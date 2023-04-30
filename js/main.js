2cadcb2b85c04bf1eb95d94c2bdfc390fe2797e1
Alexander <121974196+Lenchis21@users.noreply.github.com>
2023-05-01 00:32:14 +0300

    commit1

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const navBtn = document.querySelector('.nav-button');
+const mobileNav = document.querySelector('.mobile-nav');
+const body = document.body;
+
+
+// Клик по кнопке
+
+navBtn.addEventListener('click', function(event){
+    event.stopPropagation();
+    toggleMobileNav();
+})
+
+// Клик по окну за пределами навигации
+
+window.addEventListener('click',function(){
+    if(body.classList.contains('no-scroll')){
+        toggleMobileNav();
+    }
+})
+
+// Останавливаем клик внутри открытой навигации
+
+mobileNav.addEventListener('click', function(even){
+    event.stopPropagation();
+})
+
+function toggleMobileNav(){
+    mobileNav.classList.toggle('mobile-nav-active');
+    navBtn.classList.toggle('nav-button-close');
+    body.classList.toggle('no-scroll');
+}
